0bc036974174feffa762ca7b7ed822ac9817c9d8
HoangDucBach <hoangbach0985@gmail.com>
2024-11-17 22:56:01 +0700

    config: deploy on taiko by blockscout

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,16 +1,36 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 
 import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   defaultNetwork: "taiko",
+  etherscan: {
+    apiKey: {
+      'taiko': 'empty'
+    },
+    customChains: [
+      {
+        chainId: 167009,
+        network: "taiko",
+        urls: {
+          apiURL: 'https://blockscoutapi.hekla.taiko.xyz/api',
+          browserURL: "https://explorer.hekla.taiko.xyz"
+        }
+      }
+    ]
+  },
   networks: {
     taiko: {
       url: process.env.TAIKO_RPC_URL,
       accounts: [process.env.ACCOUNT_PRIVATE_KEY as string],
+      chainId: 167009
     }
+  },
+  sourcify: {
+    enabled: false
   }
 };
 
